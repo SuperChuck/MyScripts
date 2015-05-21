ee41b16b755d2032f30e2977e0243ba77044cab6
Chuck <chen.changge@163.com>
2015-05-21 10:12:11 +0800

    store the splited into folders and move the source file with them

--- cutting.py
+++ cutting.py
@@ -1,13 +1,27 @@
 from PIL import Image
 import os, sys
+import shutil
 
-im = Image.open(sys.argv[1])
-print(im.format, im.size, im.mode)
+filenames = os.listdir('.')
 
-times = int(im.size[1]/(im.size[0]*2))
+for filename in filenames:
+    filenameL = filename.lower()
+    if filenameL.endswith(".jpg") or filenameL.endswith(".png"):
+        im = Image.open(filename)
+        
+        if im.size[1] / im.size[0] < 5:
+            print(im.format, im.size, im.mode, end=". ")
+            print("This pic does not need split...")
+            continue
+        print(im.format, im.size, im.mode)
+        if not os.path.exists(filename[:-4]):
+            os.makedirs(filename[:-4])
+        
+        times = int(im.size[1]/(im.size[0]*2))
 
-for i in range(times):
-    outfile = sys.argv[1][:-4] + '_' + str(i+1) + '.jpg'
-    box = (0, i*2*im.size[0], im.size[0], (i+1)*2*im.size[0] + 30 )
-    region  = im.crop(box)
-    region.save(outfile, 'JPEG')
+        for i in range(times):
+            outfile = os.path.join(filename[:-4] , str(i+1) + '.jpg')
+            box = (0, i*2*im.size[0], im.size[0], (i+1)*2*im.size[0] + 30 )
+            region  = im.crop(box)
+            region.save(outfile, 'JPEG')
+        shutil.move(filename, os.path.join(filename[:-4], filename))
