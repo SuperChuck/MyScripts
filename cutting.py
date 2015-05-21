f8385cb4d6fd2c4405190b7fdd77759b6302eedf
Chuck <chen.changge@163.com>
2015-05-21 10:28:49 +0800

    add the python executing file path

--- cutting.py
+++ cutting.py
@@ -1,27 +1,33 @@
+#!C:\Python34\python.exe
 from PIL import Image
 import os, sys
 import shutil
 
-filenames = os.listdir('.')
+def main():
+    filenames = os.listdir('.')
 
-for filename in filenames:
-    filenameL = filename.lower()
-    if filenameL.endswith(".jpg") or filenameL.endswith(".png"):
-        im = Image.open(filename)
+    for filename in filenames:
+        filenameL = filename.lower()
+        if filenameL.endswith(".jpg") or filenameL.endswith(".png"):
+            im = Image.open(filename)
         
-        if im.size[1] / im.size[0] < 5:
-            print(im.format, im.size, im.mode, end=". ")
-            print("This pic does not need split...")
-            continue
-        print(im.format, im.size, im.mode)
-        if not os.path.exists(filename[:-4]):
-            os.makedirs(filename[:-4])
+            if im.size[1] / im.size[0] < 5:
+                print(im.format, im.size, im.mode, end=". ")
+                print("This pic does not need split...")
+                continue
+            print(im.format, im.size, im.mode)
+            if not os.path.exists(filename[:-4]):
+                os.makedirs(filename[:-4])
         
-        times = int(im.size[1]/(im.size[0]*2))
+            times = int(im.size[1]/(im.size[0]*2))
 
-        for i in range(times):
-            outfile = os.path.join(filename[:-4] , str(i+1) + '.jpg')
-            box = (0, i*2*im.size[0], im.size[0], (i+1)*2*im.size[0] + 30 )
-            region  = im.crop(box)
-            region.save(outfile, 'JPEG')
-        shutil.move(filename, os.path.join(filename[:-4], filename))
+            for i in range(times):
+                outfile = os.path.join(filename[:-4] , str(i+1) + '.jpg')
+                box = (0, i*2*im.size[0], im.size[0], (i+1)*2*im.size[0] + 30 )
+                region  = im.crop(box)
+                region.save(outfile, 'JPEG')
+            shutil.move(filename, os.path.join(filename[:-4], filename))
+
+
+if '__main__' == __name__:
+    main()
